da61e68cf1467c9e0d6368e34d5eb9dc390364d7
alfeukramer <alfeukramer@gmail.com>
2023-02-11 17:00:11 -0300

    feat: implementa busca pela timestamp

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import find_news
+from datetime import datetime
 
 
 # Requisito 7
@@ -12,15 +13,28 @@ def search_by_title(title):
     ]
 
 
+def convert_date(date):
+    try:
+        date_obj = datetime.strptime(date, '%Y-%m-%d')
+        return date_obj.strftime('%d/%m/%Y')
+    except ValueError:
+        raise ValueError("Data inválida")
+
+
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    data = find_news()
+    converted_data = convert_date(date)
+    return [
+            (item['title'], item['url'])
+            for item in data
+            if converted_data in item['timestamp']
+        ]
 
 
 # Requisito 9
 def search_by_category(category):
     data = find_news()
-    print(data)
 
     return [
         (item['title'], item['url'])
