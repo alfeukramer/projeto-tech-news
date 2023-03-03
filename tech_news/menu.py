551c147aba110fc7f927a11dfc898c5b86208921
alfeukramer <alfeukramer@gmail.com>
2023-03-03 16:33:35 -0300

    commit para gh

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,3 +1,18 @@
 # Requisitos 11 e 12
 def analyzer_menu():
-    """Seu código deve vir aqui"""
+    options = int(input("Selecione uma das opções a seguir: \n" +
+                        " 0 - Popular o banco com notícias;\n" +
+                        " 1 - Buscar notícias por título;\n" +
+                        " 2 - Buscar notícias por data;\n" +
+                        " 3 - Buscar notícias por categoria;\n" +
+                        " 4 - Listar top 5 categorias;\n" +
+                        " 5 - Sair.\n"))
+
+    if options == 5:
+        return None
+
+    if options == 0:
+        first_option = input("Digite quantas notícias serão buscadas:")
+
+
+print(analyzer_menu())
